95844076253638cf88bce04168b1dc802ef5dc93
Ting Feng <tfeng@sfsu.edu>
2023-05-16 16:51:00 -0700

    made some changes and finished handlebars. Milestone two completed

--- application/routes/index.js
+++ application/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'CSC 317 App', name:" Christine " });
 });
 
+router.get('/login.html', function(req, res, next) {
+  res.render('login', { title: 'CSC 317 App', name:" Christine " });
+});
+
+router.get('/postvideo.html', function(req, res, next) {
+  res.render('postvideo', { title: 'CSC 317 App', name:" Christine " });
+});
+
+router.get('/index.html', function(req, res, next) {
+  res.render('index', { title: 'CSC 317 App', name:" Christine " });
+});
+
+router.get('/viewpost.html', function(req, res, next) {
+  res.render('viewpost', { title: 'CSC 317 App', name:" Christine " });
+});
+
+router.get('/profile.html', function(req, res, next) {
+  res.render('profile', { title: 'CSC 317 App', name:" Christine " });
+});
+
+router.get('/registration.html', function(req, res, next) {
+  res.render('registration', { title: 'CSC 317 App', name:" Christine " });
+});
+
+
+
+
+
 module.exports = router;
